1744580877b4af3a720d0e5a515a4f3827e55ea0
arksu <ark@ark.su>
2019-10-13 00:09:26 +0500

    base render

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,35 @@ import ApplicationOptions = PIXI.ApplicationOptions;
 
 window._ = _;
 
-let opt : ApplicationOptions =  {
-    width: 400,
-    height : 300,
-    view:  <HTMLCanvasElement>document.getElementById("game")
+window.onload = function () {
+
+    let canvas = <HTMLCanvasElement>document.getElementById("game");
+
+    let gameWidth = window.innerWidth;
+    let gameHeight = window.innerHeight;
+    console.log("game: " + gameWidth + " x " + gameWidth);
+
+    let opt: ApplicationOptions = {
+        width: gameWidth,
+        height: gameHeight,
+        backgroundColor: 0x000000,
+        view: canvas
+    };
+
+    const app = new PIXI.Application(opt);
+    app.renderer.autoResize = true;
+
+    let rectangle = new PIXI.Graphics();
+    rectangle.beginFill(0x66CCFF);
+    rectangle.lineStyle(4, 0xFF3300, 1);
+    rectangle.drawRect(10, 20, 200, 100);
+    rectangle.endFill();
+
+    app.stage.addChild(rectangle);
+
+    window.onresize = function () {
+        app.renderer.resize(window.innerWidth, window.innerHeight);
+    };
+
+    console.log("yay!");
 };
-
-const app = new PIXI.Application(opt);
-
-let rectangle = new PIXI.Graphics();
-rectangle.beginFill(0x66CCFF);
-rectangle.lineStyle(4, 0xFF3300, 1);
-rectangle.drawRect(10,20, 200,100);
-rectangle.endFill();
-
-app.stage.addChild(rectangle);
-
-console.log("yay!");
