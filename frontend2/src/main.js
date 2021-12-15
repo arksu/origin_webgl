2f310f799a9782c30a71d67ed66f9ac92bf30572
arksu <ark@ark.su>
2021-12-16 00:25:46 +0500

    added scss

--- frontend2/src/main.js
+++ frontend2/src/main.js
@@ -1,2 +1,4 @@
+import "./scss/main.scss"
+
 console.log("test!");
 
--- frontend2/webpack.config.js
+++ frontend2/webpack.config.js
@@ -14,12 +14,27 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
     devServer: {
         historyApiFallback: true,
         client: {
             progress: true,
         },
-        static : ['assets'],
+        static: ['assets'],
         host: '0.0.0.0',
         port: 3080,
         proxy: {
