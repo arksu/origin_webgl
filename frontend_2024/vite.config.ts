f8a62ef0db82d4aa4b6d7d90e375b97bde35dc45
arksu <ark@ark.su>
2024-07-02 23:20:21 +0500

    optimize build for production

--- frontend_2024/vite.config.ts
+++ frontend_2024/vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   plugins: [
     vue()
   ],
-  // publicDir: 'assets',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -18,18 +17,42 @@ export default defineConfig({
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        manualChunks: {}
+        manualChunks: (id) => {
+          if (id) {
+            const p = id.split('node_modules/')[1]
+            if (p) {
+              if (p.includes('vue')) {
+                return 'vue'
+              } else if (p.includes('pixi.js/lib/scene')) {
+                return 'pixi-0'
+              } else if (p.includes('pixi.js/lib/rendering')) {
+                return 'pixi-1'
+              } else if (p.includes('@pixi')) {
+                return 'pixi-2'
+              } else if (p.includes('pixi.js')) {
+                return 'pixi'
+              } else {
+                return 'ext'
+              }
+            }
+          }
+
+          return 'vendor'
+        }
       }
     }
   },
-  server: {
-    port: 3070,
-    proxy: {
-      '/api': 'http://0.0.0.0:8110',
-      '/api/game': {
-        target: 'ws://0.0.0.0:8110',
-        ws: true
-      }
-    },
-  }
+  server:
+    {
+      port: 3070,
+      proxy:
+        {
+          '/api': 'http://0.0.0.0:8110',
+          '/api/game':
+            {
+              target: 'ws://0.0.0.0:8110',
+              ws: true
+            }
+        }
+    }
 })
